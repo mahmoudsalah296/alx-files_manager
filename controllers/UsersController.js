91357e1b77475192d74863d4b2fef9efe82872a6
Mahmoud Salah <ms685116@gmail.com>
2024-05-15 20:34:14 +0300

    files manager

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -24,9 +24,8 @@ export default class UsersController {
       res.status(400).json({ error: 'Already exist' });
       return;
     }
-    const insertionInfo = await (
-      await dbClient.usersCollection()
-    ).insertOne({ email, password: sha1(password) });
+    const insertionInfo = await (await dbClient.usersCollection())
+      .insertOne({ email, password: sha1(password) });
     const userId = insertionInfo.insertedId.toString();
 
     userQueue.add({ userId });
